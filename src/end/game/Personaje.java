e964506055fc54b31abf2746041b5511ddbec766
alexs <alexs@alexsachias09>
2024-04-21 17:57:08 -0600

    para el profesor alfonso

---
+++ src/end/game/Personaje.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package end.game;
+
+/**
+ *
+ * @author alexs
+ */
+
+    public class Personaje {
+  protected String nombre;
+  protected String poder;
+
+public Personaje(String nombre , String poder){
+  this.nombre=nombre;
+  this.poder=poder;
+}  
+
+public void atacar(){
+  //metodo vacio porque sera sobre escrito
+}
+}
+
