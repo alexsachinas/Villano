c096a90c4a7709cb6b035bd9b38e71d983b8a022
alexs <alexs@alexsachias09>
2024-04-21 17:58:26 -0600

    para el profesor alfonso

---
+++ src/end/game/Thanos.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package end.game;
+
+/**
+ *
+ * @author alexs
+ */
+
+    public class Thanos extends Villano {
+ protected String guantelete;
+ 
+ public Thanos(String nombre,String poder,String ejercito,String guantelete){
+  super(nombre,poder,ejercito);
+  this.guantelete=guantelete;
+ }
+ 
+  public void chasquearDedos(){
+   System.out.println(nombre + " utiliza el guantelete" + guantelete);
+  }
+  
+ }
+
